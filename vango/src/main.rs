2ec0d34adb5aeab88ac22c3463589806525ece44
Nick Marks <nmarks99@gmail.com>
2023-06-23 00:28:42 -0500

    trying to add interrupts again

--- vango/src/main.rs
+++ vango/src/main.rs
@@ -13,26 +13,43 @@ use esp_idf_hal::ledc::LedcTimerDriver;
 use esp_idf_hal::ledc::LedcDriver;
 use esp_idf_hal::ledc::config::TimerConfig;
 use esp_idf_svc::systime::EspSystemTime;
+use critical_section::Mutex;
 
 mod utils;
 mod encoder;
-
 use encoder::Encoder;
 
-// these values were obtained experimentally
-// const POT_MIN: u32 = 128;
-// const POT_MAX: u32 = 3139;
-// const SERVO_MIN: u32 = 5;
-// const SERVO_MAX: u32 = 32;
+const GEAR_RATIO: u32 = 150;
+const ENCODER_MULT: u32 = 14;
+
+
+
+// use esp_idf_sys::{
+//     esp, gpio_config, gpio_config_t, gpio_install_isr_service, gpio_int_type_t_GPIO_INTR_POSEDGE,
+//     gpio_isr_handler_add, gpio_mode_t_GPIO_MODE_INPUT, xQueueGenericCreate, xQueueGiveFromISR,
+//     xQueueReceive, QueueHandle_t, ESP_INTR_FLAG_IRAM,
+// };
+// use std::ptr;
+
+// // This `static mut` holds the queue handle we are going to get from `xQueueGenericCreate`.
+// // This is unsafe, but we are careful not to enable our GPIO interrupt handler until after this value has been initialised, and then never modify it again
+// static mut EVENT_QUEUE: Option<QueueHandle_t> = None;
+
+// #[link_section = ".iram0.text"]
+// unsafe extern "C" fn my_interrupt(_: *mut core::ffi::c_void) {
+//     xQueueGiveFromISR(EVENT_QUEUE.unwrap(), std::ptr::null_mut());
+// }
+
 
 
 fn main() -> anyhow::Result<()> {
     esp_idf_sys::link_patches();
     let peripherals = Peripherals::take().unwrap();
-    
-    // this is the builtin LED
+
+    // builtin LED
     let mut led = PinDriver::output(peripherals.pins.gpio25)?;
 
+    // system timer to get uptime
     let sys_timer = EspSystemTime {};
 
     // configure PWM pin on GPIO2
@@ -56,41 +73,33 @@ fn main() -> anyhow::Result<()> {
     let encoder = Encoder::new(peripherals.pcnt0, &mut enc_a, &mut enc_b)?;
 
     let mut enc_last = 0i64;
-    
-    pwm_pin.set_duty(max_duty)?;
+
+    // set max speed
+    pwm_pin.set_duty(max_duty/2)?;
+
+  // uint32_t currA = micros();
+  // if (lastA < currA) {
+    // // did not wrap around
+    // float rev = currA - lastA;  // us
+    // rev = 1.0 / rev;            // rev per us
+    // rev *= 1000000;             // rev per sec
+    // rev *= 60;                  // rev per min
+    // rev /= GEARING;             // account for gear ratio
+    // rev /= ENCODERMULT;         // account for multiple ticks per rotation
+    // RPM = rev;
+  // }
+  // lastA = currA;
 
     loop {
-        
-        let value = encoder.get_value()?;
-        if value != enc_last {
-            println!("value: {}, time = {}", value, sys_timer.now().as_millis());
-            enc_last = value;
-        }
-        FreeRtos::delay_ms(100u32);
-        
-        // for duty in (0..max_duty).step_by(20) {
-            // let enc_value = encoder.get_value()?;
-            // if enc_value != enc_last {
-                // println!("value: {enc_value}, duty: {duty} ");
-                // enc_last = enc_value;
-            // }
-            // led.set_high()?;
-            // pwm_pin.set_duty(duty)?;
-            // FreeRtos::delay_ms(1000);
-        // }
-//
-        // for duty in (0..max_duty+20).rev().step_by(20) {
-            // let enc_value = encoder.get_value()?;
-            // if enc_value != enc_last {
-                // println!("value: {enc_value}, duty: {duty} ");
-                // enc_last = enc_value;
-            // }
-            // led.set_low()?;
-            // pwm_pin.set_duty(duty)?;
-            // FreeRtos::delay_ms(1000);
-        // }
 
-        FreeRtos::delay_ms(1);
+        let enc_count = encoder.get_value()?;
+        if enc_count != enc_last {
+            enc_last = enc_count;
+            println!("Count = {}", enc_count);
+        }
+
+        FreeRtos::delay_ms(100);
+
     }
 
 }
