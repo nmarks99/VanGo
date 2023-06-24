bf0565d833c668f5fd7fef2aec64952858c66ebc
Nick Marks <nmarks99@gmail.com>
2023-06-23 19:55:14 -0500

    actually got an ISR working I think!

--- vango/src/main.rs
+++ vango/src/main.rs
@@ -2,7 +2,7 @@ use esp_idf_hal::peripherals::Peripherals;
 use esp_idf_hal::prelude::*;
 use esp_idf_hal::delay::FreeRtos;
 
-// use esp_idf_hal::gpio;
+use esp_idf_hal::gpio;
 use esp_idf_hal::gpio::PinDriver;
 
 // use esp_idf_hal::adc;
@@ -12,44 +12,30 @@ use esp_idf_hal::gpio::PinDriver;
 use esp_idf_hal::ledc::LedcTimerDriver;
 use esp_idf_hal::ledc::LedcDriver;
 use esp_idf_hal::ledc::config::TimerConfig;
-use esp_idf_svc::systime::EspSystemTime;
-use critical_section::Mutex;
+// use esp_idf_svc::systime::EspSystemTime;
+// use esp_idf_svc::notify::EspNotify;
+// use critical_section::{Mutex, CriticalSection};
+use core::sync::atomic::{AtomicU32, Ordering};
 
 mod utils;
-mod encoder;
-use encoder::Encoder;
+// mod encoder;
+// use encoder::Encoder;
 
-const GEAR_RATIO: u32 = 150;
-const ENCODER_MULT: u32 = 14;
-
-
-// use esp_idf_sys::{
-//     esp, gpio_config, gpio_config_t, gpio_install_isr_service, gpio_int_type_t_GPIO_INTR_POSEDGE,
-//     gpio_isr_handler_add, gpio_mode_t_GPIO_MODE_INPUT, xQueueGenericCreate, xQueueGiveFromISR,
-//     xQueueReceive, QueueHandle_t, ESP_INTR_FLAG_IRAM,
-// };
-// use std::ptr;
-
-// // This `static mut` holds the queue handle we are going to get from `xQueueGenericCreate`.
-// // This is unsafe, but we are careful not to enable our GPIO interrupt handler until after this value has been initialised, and then never modify it again
-// static mut EVENT_QUEUE: Option<QueueHandle_t> = None;
-
-// #[link_section = ".iram0.text"]
-// unsafe extern "C" fn my_interrupt(_: *mut core::ffi::c_void) {
-//     xQueueGiveFromISR(EVENT_QUEUE.unwrap(), std::ptr::null_mut());
-// }
+// const GEAR_RATIO: u32 = 150;
+// const ENCODER_MULT: u32 = 14;
 
 
+static VALUE_ATOMIC: AtomicU32 = AtomicU32::new(0);
 
 fn main() -> anyhow::Result<()> {
     esp_idf_sys::link_patches();
     let peripherals = Peripherals::take().unwrap();
 
     // builtin LED
-    let mut led = PinDriver::output(peripherals.pins.gpio25)?;
+    // let mut led = PinDriver::output(peripherals.pins.gpio25)?;
 
     // system timer to get uptime
-    let sys_timer = EspSystemTime {};
+    // let sys_timer = EspSystemTime {};
 
     // configure PWM pin on GPIO2
     let mut pwm_pin = LedcDriver::new(
@@ -66,38 +52,29 @@ fn main() -> anyhow::Result<()> {
     let mut motor_dir = PinDriver::output(peripherals.pins.gpio14)?;
     motor_dir.set_low()?;
 
-    // configure GPIO13 and GPIO12 to be inputs for encoder A and B
-    let mut enc_a = peripherals.pins.gpio13;
-    let mut enc_b = peripherals.pins.gpio12;
-    let encoder = Encoder::new(peripherals.pcnt0, &mut enc_a, &mut enc_b)?;
+    // set up interrupt on enc A
+    let mut enc_a_driver = PinDriver::input(peripherals.pins.gpio13)?;
+    let pin_number = enc_a_driver.pin(); 
 
-    let mut enc_last = 0i64;
-
-    // set max speed
+    enc_a_driver.set_pull(gpio::Pull::Up)?;
+    enc_a_driver.set_interrupt_type(gpio::InterruptType::AnyEdge)?;
+    
+    
+    // ISR
+    unsafe {
+        enc_a_driver.subscribe(move || {
+            // let new_value = esp_idf_sys::gpio_get_level(pin_number) as u32;
+            VALUE_ATOMIC.store(new_value, Ordering::SeqCst);
+        })?;
+    }
+    
+    // set motor speed
     pwm_pin.set_duty(max_duty/2)?;
-
-  // uint32_t currA = micros();
-  // if (lastA < currA) {
-    // // did not wrap around
-    // float rev = currA - lastA;  // us
-    // rev = 1.0 / rev;            // rev per us
-    // rev *= 1000000;             // rev per sec
-    // rev *= 60;                  // rev per min
-    // rev /= GEARING;             // account for gear ratio
-    // rev /= ENCODERMULT;         // account for multiple ticks per rotation
-    // RPM = rev;
-  // }
-  // lastA = currA;
-
+    
     loop {
-
-        let enc_count = encoder.get_value()?;
-        if enc_count != enc_last {
-            enc_last = enc_count;
-            println!("Count = {}", enc_count);
-        }
-
-        FreeRtos::delay_ms(100);
+        FreeRtos::delay_ms(50);
+        let value = VALUE_ATOMIC.load(Ordering::SeqCst);
+        println!("Value = {}", value);
 
     }
 
