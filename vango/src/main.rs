cf145844e5a94d6756520760e563693e8ef4c3e2
Nick Marks <nmarks99@gmail.com>
2023-06-27 11:27:43 -0500

    add neopixel library and test in main

--- vango/src/main.rs
+++ vango/src/main.rs
@@ -1,25 +1,38 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+
+// basics
+use core::sync::atomic::{AtomicBool, AtomicU32, Ordering};
+use esp_idf_hal::delay::FreeRtos;
 use esp_idf_hal::peripherals::Peripherals;
 use esp_idf_hal::prelude::*;
-use esp_idf_hal::delay::FreeRtos;
 
+// GPIO
 use esp_idf_hal::gpio;
 use esp_idf_hal::gpio::PinDriver;
 
+// ADC
 use esp_idf_hal::adc;
 use esp_idf_hal::adc::AdcChannelDriver;
 use esp_idf_hal::adc::AdcDriver;
 use esp_idf_hal::adc::Atten11dB;
 
-use esp_idf_hal::ledc::LedcTimerDriver;
-use esp_idf_hal::ledc::LedcDriver;
+// LEDC (PWM)
 use esp_idf_hal::ledc::config::TimerConfig;
-use esp_idf_svc::systime::EspSystemTime;
-// use esp_idf_svc::notify::EspNotify;
-use core::sync::atomic::{AtomicU32, AtomicBool, Ordering};
+use esp_idf_hal::ledc::LedcDriver;
+use esp_idf_hal::ledc::LedcTimerDriver;
 
+// TIME
+use esp_idf_svc::systime::EspSystemTime;
+
+// RMT
+use esp_idf_hal::rmt;
+
+// user modules
+mod neopixel;
+use neopixel::Rgb;
 mod utils;
-// mod encoder;
-// use encoder::Encoder;
 
 const GEAR_RATIO: u32 = 150;
 const ENCODER_MULT: u32 = 14;
@@ -36,8 +49,13 @@ fn main() -> anyhow::Result<()> {
     esp_idf_sys::link_patches();
     let peripherals = Peripherals::take().unwrap();
 
-    // builtin LED
-    // let mut led = PinDriver::output(peripherals.pins.gpio25)?;
+    // neopixel on GPIO12
+    let rmt_config = rmt::config::TransmitConfig::new().clock_divider(1);
+    let mut rmt_tx = rmt::TxRmtDriver::new(
+        peripherals.rmt.channel0,
+        peripherals.pins.gpio12,
+        &rmt_config,
+    )?;
 
     // system timer to get uptime
     let sys_timer = EspSystemTime {};
@@ -49,14 +67,14 @@ fn main() -> anyhow::Result<()> {
             peripherals.ledc.timer0,
             &TimerConfig::new().frequency(50.Hz().into()),
         )?,
-        peripherals.pins.gpio16
+        peripherals.pins.gpio16,
     )?;
     let max_duty = pwm_pin.get_max_duty();
 
     // Setup analog input for potentiometer
     let mut adc_driver = AdcDriver::new(
         peripherals.adc2,
-        &adc::config::Config::new().calibration(true)
+        &adc::config::Config::new().calibration(true),
     )?;
     let mut pot: AdcChannelDriver<'_, gpio::Gpio4, Atten11dB<_>> =
         adc::AdcChannelDriver::new(peripherals.pins.gpio4)?;
@@ -78,7 +96,7 @@ fn main() -> anyhow::Result<()> {
             let current_time = sys_timer.now().as_micros() as u32;
             let last_time = LAST_TIME_ATOMIC.load(Ordering::SeqCst);
             if last_time < current_time {
-                ELAPSED_TIME_ATOMIC.store(current_time-last_time, Ordering::SeqCst);
+                ELAPSED_TIME_ATOMIC.store(current_time - last_time, Ordering::SeqCst);
             }
             LAST_TIME_ATOMIC.store(current_time, Ordering::SeqCst);
         })?;
@@ -88,39 +106,49 @@ fn main() -> anyhow::Result<()> {
     pwm_pin.set_duty(0)?;
 
     loop {
-        FreeRtos::delay_ms(50);
-        let pot_val = adc_driver.read(&mut pot).unwrap();
-        let duty = utils::map(pot_val.into(), POT_MIN, POT_MAX, 0, max_duty);
-        pwm_pin.set_duty(duty.into())?;
-        let rpm = get_motor_rpm();
-        println!("duty = {}%, speed = {} rpm", (100*duty/max_duty), rpm);
-    }
+        neopixel::set_color("red", &mut rmt_tx)?;
+        FreeRtos::delay_ms(200);
 
+        neopixel::set_color("green", &mut rmt_tx)?;
+        FreeRtos::delay_ms(200);
+
+        neopixel::set_color("blue", &mut rmt_tx)?;
+        FreeRtos::delay_ms(200);
+
+        neopixel::set_color("yellow", &mut rmt_tx)?;
+        FreeRtos::delay_ms(200);
+
+        neopixel::set_color("cyan", &mut rmt_tx)?;
+        FreeRtos::delay_ms(200);
+
+        neopixel::set_color("purple", &mut rmt_tx)?;
+        FreeRtos::delay_ms(200);
+
+        // FreeRtos::delay_ms(100);
+        // let pot_val = adc_driver.read(&mut pot).unwrap();
+        // let duty = utils::map(pot_val.into(), POT_MIN, POT_MAX, 0, max_duty);
+        // pwm_pin.set_duty(duty.into())?;
+        // let rpm = get_motor_rpm();
+        // println!("duty = {}%, speed = {} rpm", (100 * duty / max_duty), rpm);
+    }
 }
 
 // Calculates motor speed by doing an atomic read of
 // the elapsed time between encoder interrupts
 fn get_motor_rpm() -> f32 {
-    let mut rpm: f32; 
+    let mut rpm: f32;
     if ENC_INTERRUPT_FLAG.load(Ordering::SeqCst) {
         let elapsed = ELAPSED_TIME_ATOMIC.load(Ordering::SeqCst);
         if elapsed == 0 {
             rpm = 0.0
-        }
-        else {
+        } else {
             rpm = 1.0 / (elapsed as f32);
             rpm = rpm * 1000000.0 * 60.0 / GEAR_RATIO as f32 / ENCODER_MULT as f32;
         }
-        ENC_INTERRUPT_FLAG.store(false,Ordering::SeqCst);
-    }
-    else {
+        ENC_INTERRUPT_FLAG.store(false, Ordering::SeqCst);
+    } else {
         rpm = 0.0;
     }
 
     rpm
 }
-
-
-
-
-
---
+++ vango/src/neopixel.rs
@@ -0,0 +1,98 @@
+// Code taken from https://github.com/esp-rs/esp-idf-hal/blob/master/examples/rmt_neopixel.rs
+
+use anyhow;
+use core::time::Duration;
+use esp_idf_hal::gpio;
+use esp_idf_hal::peripherals;
+use esp_idf_hal::rmt;
+use esp_idf_hal::rmt::PinState;
+use esp_idf_hal::rmt::TxRmtDriver;
+use esp_idf_hal::rmt::{FixedLengthSignal, Pulse};
+
+// sets the neopixel to the specified RGB
+pub fn set_rgb(rgb: Rgb, tx: &mut TxRmtDriver) -> anyhow::Result<()> {
+    let color: u32 = rgb.into();
+    let ticks_hz = tx.counter_clock()?;
+    let (t0h, t0l, t1h, t1l) = (
+        Pulse::new_with_duration(ticks_hz, PinState::High, &Duration::from_nanos(350))?,
+        Pulse::new_with_duration(ticks_hz, PinState::Low, &Duration::from_nanos(800))?,
+        Pulse::new_with_duration(ticks_hz, PinState::High, &Duration::from_nanos(700))?,
+        Pulse::new_with_duration(ticks_hz, PinState::Low, &Duration::from_nanos(600))?,
+    );
+    let mut signal = FixedLengthSignal::<24>::new();
+    for i in (0..24).rev() {
+        let p = 2_u32.pow(i);
+        let bit: bool = p & color != 0;
+        let (high_pulse, low_pulse) = if bit { (t1h, t1l) } else { (t0h, t0l) };
+        signal.set(23 - i as usize, &(high_pulse, low_pulse))?;
+    }
+    tx.start_blocking(&signal)?;
+    Ok(())
+}
+
+// set color from a string
+// if the provided color is not implemented, do nothing
+pub fn set_color(color: &str, tx: &mut TxRmtDriver) -> anyhow::Result<()> {
+    let rgb_op: Option<Rgb> = match color {
+        "red" => Some(Rgb::new(255, 0, 0)),
+        "green" => Some(Rgb::new(0, 255, 0)),
+        "blue" => Some(Rgb::new(0, 0, 255)),
+        "purple" => Some(Rgb::new(255, 0, 255)),
+        "yellow" => Some(Rgb::new(255, 255, 0)),
+        "cyan" => Some(Rgb::new(0, 255, 255)),
+        "white" => Some(Rgb::new(50, 50, 50)),
+        _ => None,
+    };
+    if rgb_op.is_some() {
+        set_rgb(rgb_op.unwrap(), tx)?;
+    }
+    Ok(())
+}
+
+pub struct Rgb {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+impl Rgb {
+    pub fn new(r: u8, g: u8, b: u8) -> Self {
+        Self { r, g, b }
+    }
+    /// Converts hue, saturation, value to RGB
+    pub fn from_hsv(h: u32, s: u32, v: u32) -> anyhow::Result<Self> {
+        if h > 360 || s > 100 || v > 100 {
+            anyhow::bail!("The given HSV values are not in valid range");
+        }
+        let s = s as f64 / 100.0;
+        let v = v as f64 / 100.0;
+        let c = s * v;
+        let x = c * (1.0 - (((h as f64 / 60.0) % 2.0) - 1.0).abs());
+        let m = v - c;
+        let (r, g, b) = match h {
+            0..=59 => (c, x, 0.0),
+            60..=119 => (x, c, 0.0),
+            120..=179 => (0.0, c, x),
+            180..=239 => (0.0, x, c),
+            240..=299 => (x, 0.0, c),
+            _ => (c, 0.0, x),
+        };
+        Ok(Self {
+            r: ((r + m) * 255.0) as u8,
+            g: ((g + m) * 255.0) as u8,
+            b: ((b + m) * 255.0) as u8,
+        })
+    }
+}
+
+impl From<Rgb> for u32 {
+    /// Convert RGB to u32 color value
+    ///
+    /// e.g. rgb: (1,2,4)
+    /// G        R        B
+    /// 7      0 7      0 7      0
+    /// 00000010 00000001 00000100
+    fn from(rgb: Rgb) -> Self {
+        ((rgb.r as u32) << 16) | ((rgb.g as u32) << 8) | rgb.b as u32
+    }
+}
