5674449ae82a41c026ce3ba430a53241c23dcc64
Nick Marks <nmarks99@gmail.com>
2023-07-06 11:49:29 -0500

    refactor some of pen.rs, remove motor.rs for now
    need to rewrite motor.rs similar to pen.rs and inlude PID
    controller in there

--- vango/src/pen.rs
+++ vango/src/pen.rs
@@ -19,12 +19,40 @@ pub enum PenState {
     DOWN
 }
 
+/// Pen object which provides methods for moving a servo
+/// between "UP" and "DOWN" states
 pub struct Pen<'a> {
     ledc_driver: LedcDriver<'a>,
     state: PenState
 }
 
 impl<'a> Pen<'a> {
+    /// Returns a Pen configured on the specified pin
+    ///
+    /// # Arguments
+    ///
+    /// * `pin` - The PWM pin the servo is connected to
+    /// * `ledc_channel` - The LedcChannel to use
+    /// * `ledc_timer` - The LedcTimer to use
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use doc::Pen;
+    /// use esp_idf_hal::peripherals::Peripherals;
+    ///
+    /// fn main() -> anyhow::Result() {
+    /// let mut pen = Pen::new(
+    ///    peripherals.pins.gpio15,
+    ///    peripherals.ledc.channel2,
+    ///    peripherals.ledc.timer2
+    /// )?;
+    /// pen.up()?;
+    /// let state = pen.get_state()?
+    /// }
+    ///
+    /// ```
+
     pub fn new(
         pin: impl Peripheral<P = impl OutputPin> + 'a,
         ledc_channel: impl Peripheral<P = impl LedcChannel> + 'a,
@@ -39,20 +67,35 @@ impl<'a> Pen<'a> {
         Ok(Self { ledc_driver, state: PenState::UP })
     }
 
+    /// Moves the pen up
     pub fn up(&mut self) -> anyhow::Result<()> {
         self.ledc_driver.set_duty(UP_VALUE)?;
         self.state = PenState::UP;
         Ok(())
     }
 
+    /// Moves the pen down
     pub fn down(&mut self) -> anyhow::Result<()> {
         self.ledc_driver.set_duty(DOWN_VALUE)?;
         self.state = PenState::DOWN;
         Ok(())
     }
 
+    /// Returns the current state of the pen
     pub fn get_state(&self) -> anyhow::Result<PenState, EspError> {
         Ok(self.state)
     }
 
+}
+
+/// Converts a PenState into a boolean cooresponding
+/// to whether or not the pen is in contact with the surface,
+/// or in other words DOWN = true, UP = false
+impl From<PenState> for bool {
+    fn from(pen_state: PenState) -> Self {
+        match pen_state {
+            PenState::UP => false,
+            PenState::DOWN => true
+        }
+    }
 }
