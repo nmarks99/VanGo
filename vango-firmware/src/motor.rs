04ac578f92b99df08baebb2810e848bd014f9ab1
Nick Marks <nmarks99@gmail.com>
2023-08-09 23:39:03 -0500

    attempted to write motor.rs, gave up

--- vango-firmware/src/motor.rs
+++ vango-firmware/src/motor.rs
@@ -1,29 +1,37 @@
-#![allow(dead_code)]
-#![allow(unused_imports)]
-use anyhow;
-use esp_idf_hal::gpio;
-use esp_idf_hal::gpio::OutputPin;
-use esp_idf_hal::ledc;
-use esp_idf_hal::ledc::{LedcChannel, LedcDriver, LedcTimer, LedcTimerDriver};
-use esp_idf_hal::peripheral::Peripheral;
-use esp_idf_hal::prelude::*;
-use esp_idf_sys::EspError;
+// use esp_idf_hal::gpio::{self, OutputPin};
+// use esp_idf_hal::ledc;
+// use esp_idf_hal::ledc::{LedcChannel, LedcDriver, LedcTimer, LedcTimerDriver};
+// use esp_idf_hal::peripheral::Peripheral;
+// use esp_idf_hal::prelude::*;
+// use esp_idf_sys::EspError;
 
-pub struct Mototr<'a> {
-    ledc_driver: LedcDriver<'a>,
-}
+// const MOTOR_PWM_FREQ: u32 = 80; // Hz
 
-impl<'a> Motor<'a> {
-    pub fn new(
-        pin: impl Peripheral<P = impl OutputPin> + 'a,
-        ledc_channel: impl Peripheral<P = impl LedcChannel> + 'a,
-        ledc_timer: impl Peripheral<P = impl LedcTimer> + 'a,
-    ) -> Result<Self, EspError> {
-        let timer_driver = LedcTimerDriver::new(
-            ledc_timer,
-            &ledc::config::TimerConfig::new().frequency(Hertz(PWM_FREQ).into()),
-        )?;
-        let mut ledc_driver = LedcDriver::new(ledc_channel, timer_driver, pin)?;
-        Ok(Self { ledc_driver })
-    }
-}
+// pub struct Motor<'a> {
+//     ledc_driver: LedcDriver<'a>,
+//     direction_pin_driver: gpio::PinDriver<'a, gpio::AnyOutputPin, gpio::Output>,
+// }
+
+// impl<'a> Motor<'a> {
+//     pub fn new(
+//         pin: impl Peripheral<P = impl OutputPin> + 'a,
+//         ledc_channel: impl Peripheral<P = impl LedcChannel> + 'a,
+//         ledc_timer: impl Peripheral<P = impl LedcTimer> + 'a,
+//     ) -> Result<Self, EspError> {
+//         let timer_driver = LedcTimerDriver::new(
+//             ledc_timer,
+//             &ledc::config::TimerConfig::new().frequency(Hertz(MOTOR_PWM_FREQ).into()),
+//         )?;
+//         let ledc_driver = LedcDriver::new(ledc_channel, timer_driver, pin)?;
+//         let mut direction_pin_driver = gpio::PinDriver::output(pin)?;
+//         Ok(Self {
+//             ledc_driver,
+//             direction_pin_driver,
+//         })
+//     }
+
+//     pub fn set_duty(&mut self, duty: u32) -> anyhow::Result<()> {
+//         self.ledc_driver.set_duty(duty)?;
+//         Ok(())
+//     }
+// }
